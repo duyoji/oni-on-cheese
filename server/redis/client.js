16ceda1054ecb67369207a12b3fa402feb449179
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-13 16:25:54 +0900

    Changed name REDIS_URI to REDIS_URL in .env

--- server/redis/client.js
+++ server/redis/client.js
@@ -9,7 +9,7 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 const client = redis.createClient({
-  url: process.env.REDIS_URI
+  url: process.env.REDIS_URL
 });
 
 export default client;
