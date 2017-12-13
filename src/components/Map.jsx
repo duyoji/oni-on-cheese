6301609be465d6873c270bd4f0e4eb96a5893127
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-13 14:57:17 +0900

    multiple markers are shown using dummydata

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,24 +2,36 @@ import React, { Component } from "react"
 import { compose, withProps } from "recompose"
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+const dummyMarkers = [
+  { lat: 35.6641, lng: 139.7294 },
+  { lat: 35.6621, lng: 139.7254 },
+  { lat: 35.6631, lng: 139.7264 },
+  { lat: 35.6661, lng: 139.7284 },
+  { lat: 35.6681, lng: 139.7224 }
+]
+
 const MyMap = withGoogleMap(props => {
+  dummyMarkers.map((marker, index) => {
+    console.log(marker)
+  })
   return (
   <GoogleMap
     defaultZoom={15}
     defaultCenter={{ lat: 35.6641, lng: 139.7294 }}
   >
-  <Marker
-    position={{ lat: 35.6641, lng: 139.7294 }}
-  />
+  {dummyMarkers.map((marker, index) => (
+    <Marker
+      position={marker}
+      key={index}
+    />
+  ))}
   </GoogleMap>
   )
 });
-  
 
 class Map extends Component {
   constructor(props){
     super(props);
-    this.state = { isMarkerShown: false }
   }
 
   render() {
