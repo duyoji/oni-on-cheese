86cb60aebbdfe415d881a98e01ece544e58e194f
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-14 11:30:03 +0900

    Passed first test for App component

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import { expect } from 'chai';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-// http://airbnb.io/enzyme/docs/api/ShallowWrapper/contains.html
 describe('App component', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<App />);
 
     expect(wrapper.find('.App')).to.have.length(1);
   });
-
 });
