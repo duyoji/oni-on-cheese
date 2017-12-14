7da99f3bdaaf983e3c6d250955598f4553e2b377
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-14 11:17:25 +0900

    added a failing test for tests directory

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+configure({ adapter: new Adapter() });
+
+// http://airbnb.io/enzyme/docs/api/ShallowWrapper/contains.html
+describe('App component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('.App')).to.have.length(1);
+  });
+
 });
