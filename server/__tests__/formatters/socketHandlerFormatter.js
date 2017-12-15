b2468bf064ac9ab0f2b8d0eae85bc501ac7a210e
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-15 14:48:18 +0900

    Completed tests for formatOutput.

--- server/__tests__/formatters/socketHandlerFormatter.js
+++ server/__tests__/formatters/socketHandlerFormatter.js
@@ -11,5 +11,33 @@ describe('server/formatters/socketHandlerFormatter', () => {
     it('should be a function', () => {
       expect(typeof formatOutput).toEqual('function');
     });
+
+    it('throws an error when both props of `error` and `data` are passed or both are not passed to the function.', () => {
+      expect(() => {
+        formatOutput();
+      }).toThrowError('Need an error or data prop.');
+      expect(() => {
+        formatOutput({error: new Error(), data: 1});
+      }).toThrowError('Cannot pass both error and data props.');
+    });
+
+    it('should include result that includes error and data props', () => {
+      const formattedData = formatOutput({data:1});
+      expect(formattedData.hasOwnProperty('result')).toEqual(true);
+      expect(formattedData.result.hasOwnProperty('error')).toEqual(true);
+      expect(formattedData.result.hasOwnProperty('data')).toEqual(true);
+    });
+
+    it('should return same error.', () => {
+      const error = new Error();
+      const formattedData = formatOutput({error});
+      expect(formattedData.result.error).toEqual(error);
+    });
+
+    it('should return same data.', () => {
+      const data = {data:[1,2,3,'abc']};
+      const formattedData = formatOutput({data});
+      expect(formattedData.result.data).toEqual(data);
+    });
   });
 });
--- server/formatters/socketHandlerFormatter.js
+++ server/formatters/socketHandlerFormatter.js
@@ -1,4 +1,21 @@
 const formatInput = () => {};
-const formatOutput = () => {};
+
+/**
+ *
+ * @param {Error} error
+ * @param {object} data
+ */
+const formatOutput = ({error, data} = {}) => {
+  if( (!error && !data ) ) {
+    throw new Error('Need an error or data prop.');
+  }
+  if( (error && data ) ) {
+    throw new Error('Cannot pass both error and data props.');
+  }
+
+  return {
+    result: {error, data}
+  };
+};
 
 export { formatInput, formatOutput };
