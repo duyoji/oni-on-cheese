74688a66de89d2db897356b0431a807a9462f8fc
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-15 11:26:37 +0900

    Created a test file for socketHandlers/leaveRoom.js

--- server/socketHandlers/leaveRoom.js
+++ server/socketHandlers/leaveRoom.js
@@ -3,7 +3,7 @@ const leaveRoom = (socket, socketNamespace) => {
   socket.on('leaveRoom', ({roomId}) => {
     socketNamespace.adapter.remoteLeave(socket.id, roomId, (err) => {
       if (err) return;
-      socket.to(roomId).emit('leaveRoom', `${socket.id} has left this room.`);
+      socket.to(roomId).emit('resultLeaveRoom', `${socket.id} has left this room.`);
     });
   });
 };
