33ef49a150c8534c16bad0969e37375bb685c8cf
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-17 20:25:46 +0900

    get user profile from facebook

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
+import * as hello from 'hellojs'
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+    this.onClickLogin = this.onClickLogin.bind(this);
+
+  }
+  
+  onClickLogin(){
+    hello.init({
+      facebook: '1948552805410537',
+    }, {
+      redirect_uri: '/',
+    });
+    hello('facebook').login({
+      scope: 'email'
+    },)
+      .then(() => {
+        return hello('facebook').api('me')
+      })
+      .then((res) => {
+        console.log(res);
+      })
+  }
+  
+  render(){
+    return (
+      <button onClick={this.onClickLogin}>facebook</button>
+    )
+  }
 }
 
 export default Login;
