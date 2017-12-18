932d636c449056eb0cf498ebec5e0ca83040a52e
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-18 16:34:37 +0900

    Playing around react-router-dom and reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact={true} path="/" component={TopPage} />
+          <Route exact={true} path="/login" component={LoginPage} />
+          <Route exact={true} path="/rooms" component={RoomsPage} />
+          <Route exact={true} path="/map" component={MapPage} />
+        </div>
+      </Router>
     );
   }
 }
+
+const TopPage = () => (
+  <div>
+    <p>Top Page</p>
+    <Link to='/login'>
+      <Button color="primary" size="lg" block>move to login page</Button>
+    </Link>
+  </div>
+);
+const LoginPage = () => (
+  <div>
+    <p>Login Page</p>
+    <Link to='/rooms'>
+      <Button color="secondary" size="lg" block>move to rooms page</Button>
+    </Link>
+  </div>
+);
+const RoomsPage = () => (
+  <div>
+    <p>Rooms Page</p>
+    <Link to='/map'>
+      <Button color="success" size="lg" block>move to map page</Button>
+    </Link>
+  </div>
+);
+const MapPage = () => (
+  <div>
+    <p>Map Page</p>
+    <Link to='/'>
+      <Button color="warning" size="lg" block>move to top page</Button>
+    </Link>
+  </div>
+);
 
 export default App;
