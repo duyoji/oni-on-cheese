f2ac8a40b6f132c788a48f7631f06be1d2209b19
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-18 12:29:50 +0900

    changed AppID and used env variable for client secret

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
   'facebookAuth': {
-    'clientID': '1948552805410537', // your App ID
-    'clientSecret': '56d322d1a31e944b0ba86b57983de257', // your App Secret
+    'clientID': '1723859427634595', // App ID
+    'clientSecret': process.env.clientSecret, // App Secret
     'callbackURL': 'http://localhost:8080/auth/facebook/callback',
     'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
     'profileFields': ['id', 'email', 'name'] // For requesting permissions from Facebook API
