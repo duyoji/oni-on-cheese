17739806f796c8f5b1968bbcb11ed607d6faba6c
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-18 11:10:47 +0900

    removed hellojs and added passportjs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
-import * as hello from 'hellojs'
 
 class Login extends Component {
   constructor(props){
     super(props);
-    this.onClickLogin = this.onClickLogin.bind(this);
-
-  }
-  
-  onClickLogin(){
-    hello.init({
-      facebook: '1948552805410537',
-    }, {
-      redirect_uri: '/',
-    });
-    hello('facebook').login({
-      scope: 'email'
-    },)
-      .then(() => {
-        return hello('facebook').api('me')
-      })
-      .then((res) => {
-        console.log(res);
-      })
   }
   
   render(){
     return (
-      <button onClick={this.onClickLogin}>facebook</button>
+      <button>facebook</button>
     )
   }
 }
