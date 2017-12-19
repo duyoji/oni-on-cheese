2900bb5d3781832f2e957f7cdbcae66e366308e8
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-19 15:55:52 +0900

    add currentview check feathre in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,51 +4,23 @@ import { Button } from 'reactstrap';
 import Top from './Top';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  get currentView(){
+    if (this.props.currentView === 'Top'){
+      return <Route exact={true} path="/" component={Top} />
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Route exact={true} path="/" component={Top} />
-        {/* <Route exact={true} path="/login" component={LoginPage} /> */}
-        <Route exact={true} path="/rooms" component={RoomsPage} />
-        <Route exact={true} path="/map" component={MapPage} />
+        {this.currentView}
       </div>
     );
   }
 }
-
-// TODO: Replace TopPage Container
-const TopPage = () => (
-  <Redirect to="/login"/>
-);
-
-// TODO: Login Container
-const LoginPage = () => (
-  <div className="loginPage">
-    <p>Login Page</p>
-    <Link to='/rooms'>
-      <Button color="secondary" size="lg" block>move to rooms page</Button>
-    </Link>
-  </div>
-);
-
-// TODO: RoomListPage Container
-const RoomsPage = () => (
-  <div className="roomsPage">
-    <p>Rooms Page</p>
-    <Link to='/map'>
-      <Button color="success" size="lg" block>move to map page</Button>
-    </Link>
-  </div>
-);
-
-// TODO: MapPage Container
-const MapPage = () => (
-  <div className="mapPage">
-    <p>Map Page</p>
-    <Link to='/'>
-      <Button color="warning" size="lg" block>move to top page</Button>
-    </Link>
-  </div>
-);
 
 export default App;
