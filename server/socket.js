f3d67817886e3f935ded483e163dcda88d294d1a
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-19 16:40:47 +0900

    Not use sticky-session for now because of heroku plan

--- server/socket.js
+++ server/socket.js
@@ -9,21 +9,10 @@ import { setSocketEventHandler } from './socketHandlers/index';
 dotenv.config();
 
 const listen = (expressApp, port) => {
-  // const io = socketIO();
   const server = http.createServer(expressApp);
   const io = socketIO(server);
-  // console.log(io);
-
   io.adapter(redisAdapter(process.env.REDIS_URL));
-  // io.attach(server);
 
-  // const isWorker = sticky.listen(server, port);
-
-  // if (isWorker) {
-  //   const game = io.of('/game').on('connection', (socket) => {
-  //     setSocketEventHandler(socket, game);
-  //   });
-  // }
   const game = io.of('/game').on('connection', (socket) => {
     setSocketEventHandler(socket, game);
   });
