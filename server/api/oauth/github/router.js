9ea34b53d0049a327e25dc9dbd602059d06e9d58
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-19 11:11:46 +0900

    client info in .env

--- server/api/oauth/github/router.js
+++ server/api/oauth/github/router.js
@@ -1,21 +1,24 @@
 import { Router } from 'express';
 import passport from 'passport';
 import GithubPassport from 'passport-github';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const router = Router();
 
 const GithubStrategy = GithubPassport.Strategy;
 
 passport.use(new GithubStrategy({
-    clientID: '71a62caadcdfa8c0e5e1',
+    clientID: process.env.clientIDGithub,
     clientSecret: process.env.clientSecretGithub,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
+    callbackURL: 'http://localhost:9000/auth/github/callback'
   },
   (accessToken, refreshToken, profile, done) => {
-    if (err) {
-      return done(err);
-    }
-    done(null, user);
+    // if (err) {
+    //   return done(err);
+    // }
+    done(null, profile);
   }
 ));
 
