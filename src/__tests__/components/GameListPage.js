1726f591d1d12ee2cc75d98a62081c49c72b5d08
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-20 18:50:25 +0900

    Added a test for Actual GameListPage component.

--- src/__tests__/components/GameListPage.js
+++ src/__tests__/components/GameListPage.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GameListPage from '../../components/GameListPage';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+const mockGetRooms = () => {};
+const mockJoinRoom = () => {};
+const ROOM_IDS = ['room1', 'room2', 'room3'];
+
 describe('src/components/GameListPage.jsx', () => {
   let wrapper;
+  const ROOM_IDS = ['room1', 'room2', 'room3'];
   beforeEach(() => {
-    wrapper = shallow( <GameListPage /> );
+    wrapper = shallow( <GameListPage
+      getRooms={mockGetRooms}
+      joinRoom={mockJoinRoom}
+      roomIds={ROOM_IDS}
+    /> );
   });
 
-  it('redirect to login page.', () => {
-    expect(wrapper.find(".gameListPage").length).toBe(1);
+  it('shows room list.', () => {
+    expect(wrapper.find(ListGroup).length).toEqual(1);
+    expect(wrapper.find(ListGroupItem).length).toEqual(3);
   });
 });
