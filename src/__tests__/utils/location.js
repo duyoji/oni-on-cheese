f0110a776c5aa24f04ae1e3c46c70c9f2361e3e9
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-21 12:49:41 +0900

    Create a module for getting location and test for that.

---
+++ src/__tests__/utils/location.js
@@ -0,0 +1,62 @@
+import { getCurrentPosition } from '../../utils/location';
+
+describe('src/utils/location.js', () => {
+  describe('The getLocation function', () => {
+    const DUMMY_LATITUDE = 9999;
+    const DUMMY_LONGITUDE = -9999;
+    const DUMMY_ERROR = {
+      code: 1234,
+      message: 'This is dummy error.'
+    };
+    let callbackForSuccess = ({latitude, longitude}) => {};
+    let callbackForError = (err) => {};
+    let isSuccess = true;
+
+    beforeAll(() => {
+      navigator.geolocation = navigator.geolocation || {};
+      navigator.geolocation.getCurrentPosition
+        = navigator.geolocation.getCurrentPosition || function(success, error, options) {
+          if(isSuccess) {
+            const position = {
+              latitude: DUMMY_LATITUDE,
+              longitude: DUMMY_LONGITUDE
+            };
+            callbackForSuccess(position);
+          } else {
+            callbackForError(DUMMY_ERROR);
+          }
+      };
+    });
+
+    afterAll(() => {
+      if(navigator.geolocation.getCurrentPosition.mockRestore) {
+        navigator.geolocation.getCurrentPosition.mockRestore();
+      }
+    });
+
+    it('returns location data when getting location succeeded.', (done) => {
+      // Set `true` to check success pattern.
+      isSuccess = true;
+
+      callbackForSuccess = ({latitude, longitude}) => {
+        expect(latitude).toEqual(DUMMY_LATITUDE);
+        expect(longitude).toEqual(DUMMY_LONGITUDE);
+        done();
+      };
+
+      getCurrentPosition({success: callbackForSuccess});
+    });
+
+    it('returns error object when gettingLocation failed.', (done) => {
+      // Set `false` to check error pattern.
+      isSuccess = false;
+
+      callbackForError = (err) => {
+        expect(err).toEqual(DUMMY_ERROR);
+        done();
+      };
+
+      getCurrentPosition({error: callbackForError});
+    });
+  });
+});
