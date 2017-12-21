06b1774453c320d01fc21dcd0f1237a470d147af
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-21 14:33:52 +0900

    Created a test for addHandlerListener of src/socketHandlers/updateLocation.js

--- src/__tests__/socketHandlers/updateLocation.js
+++ src/__tests__/socketHandlers/updateLocation.js
@@ -3,21 +3,23 @@ import socket from '../../socketHandlers/index';
 import sinon, { expectation } from 'sinon';
 
 describe('src/socketHandlers/updateLocation.js', () => {
+  const LATITUDE = 12345;
+  const LONGITUDE = 54321;
+  const ROOM_ID = '8u08u348ur39u4tegf';
+  const USER_ID = '209jlknq39';
+  const USER_NAME = 'NAME_DESU';
+  const ICON_URL = 'http://difasdfia.com/ijqf.png';
+
   afterAll(() => {
     socket.disconnect();
   });
 
   describe('The emit function', () => {
     it('should call socket.emit and passed args', (done) => {
-      const LATITUDE = 12345;
-      const LONGITUDE = 54321;
-      const ROOM_ID = '8u08u348ur39u4tegf';
-      const USER_ID = '209jlknq39';
-      const USER_NAME = 'NAME_DESU';
-      const ICON_URL = 'http://difasdfia.com/ijqf.png';
       const mockLocation = createMockLocation(LATITUDE, LONGITUDE);
 
       sinon.stub(socket, 'emit').callsFake((type, data) => {
+        expect(type).toEqual('updateLocation');
         expect(data.id).toEqual(USER_ID);
         expect(data.name).toEqual(USER_NAME);
         expect(data.iconUrl).toEqual(ICON_URL);
@@ -34,6 +36,38 @@ describe('src/socketHandlers/updateLocation.js', () => {
         id: USER_ID,
         name: USER_NAME,
         iconUrl: ICON_URL
+      });
+    });
+  });
+
+  describe('The addHandlerListener function', () => {
+    it('should call callback function with expected data', (done) => {
+      const mockLocation = createMockLocation(LATITUDE, LONGITUDE);
+      const parsedLocation = JSON.parse(mockLocation);
+
+      sinon.stub(socket, 'on').callsFake((type, callback) => {
+        expect(type).toEqual('resultUpdateLocation');
+        const data = {
+          result: {
+            data:{
+              id: USER_ID,
+              name: USER_NAME,
+              iconUrl: ICON_URL,
+              location: parsedLocation
+            }
+          }
+        };
+        callback(data);
+      });
+
+      addHandlerListener(({id, name, iconUrl, location}) => {
+        expect(id).toEqual(USER_ID);
+        expect(name).toEqual(USER_NAME);
+        expect(iconUrl).toEqual(ICON_URL);
+        expect(location).toEqual(parsedLocation);
+
+        socket.on.restore();
+        done();
       });
     });
   });
