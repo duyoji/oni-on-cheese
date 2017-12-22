cbdd08c17ddd4dc18cebc3a106335b56dad091f0
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-22 18:35:18 +0900

    emit user id left the room to all room

--- server/socketHandlers/disconnect.js
+++ server/socketHandlers/disconnect.js
@@ -1,7 +1,19 @@
+import { formatOutput } from '../formatters/socketHandlerFormatter';
+const EVENT_TYPES = {
+  EMIT: 'resultLeaveRoom'
+};
+
 // See: ./index.js
 const disconnect = (socket, socketNamespace) => { // eslint-disable-line no-unused-vars
   socket.on('disconnect', () => {
-    // console.log('DISCONECT', socket.id, socketNamespace);
+    const rooms = socketNamespace.adapter.rooms;
+
+    // TODO: If I can get the specific roomId where the user was, Use the roomId.
+    // For now Just emit left user id to all rooms.
+    for(let roomId in rooms) {
+      const userId = socket.id;
+      socketNamespace.in(roomId).emit(EVENT_TYPES.EMIT, formatOutput({data: {userId}}));
+    }
   });
 };
 
