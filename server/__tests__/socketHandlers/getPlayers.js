406990f36630f71ea7766a9d8b6f97c516fe0ef8
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 12:26:50 +0900

    added common tests for socket and failed

---
+++ server/__tests__/socketHandlers/getPlayers.js
@@ -0,0 +1,8 @@
+import { getPlayers } from '../../socketHandlers/getPlayers';
+import {
+  commonTestForSocketHandler,
+  createDummySocket,
+  createDummyNameSpace
+} from '../helpers/socketHandlerHelper';
+
+commonTestForSocketHandler( getPlayers );
