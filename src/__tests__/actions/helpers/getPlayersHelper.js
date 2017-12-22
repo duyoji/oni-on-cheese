ce3fb1b4889798a48635cdcc8e62dcb6a2dc485c
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 16:12:34 +0900

    status

--- src/__tests__/actions/helpers/getPlayersHelper.js
+++ src/__tests__/actions/helpers/getPlayersHelper.js
@@ -1,5 +1,12 @@
+import { getPlayersPromise } from '../../../actions/helpers/getPlayersHelper'
+import socket from '../../socketHandlers/index'
+
 describe('src/__tests__/actions/helpers/getPlayersHelper.js', () => {
+  afterAll(() => {
+    socket.disconnect();
+  });
   it('should return promise object', () => {
-    
-  })
+    const promise = getPlayersPromise();
+    expect(promise instanceof Promise).toEqual(true);
+  });
 });
--- src/actions/helpers/getPlayersHelper.js
+++ src/actions/helpers/getPlayersHelper.js
@@ -1,3 +1,4 @@
-const getPlayersPromise = () => {};
+const getPlayersPromise = () => {
+};
 
 export { getPlayersPromise };
