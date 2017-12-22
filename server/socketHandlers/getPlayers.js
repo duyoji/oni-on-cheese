4fe0e7f1a83d9ed47d4749f9c2605684c3797129
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 14:07:20 +0900

    passed test for on feature

--- server/socketHandlers/getPlayers.js
+++ server/socketHandlers/getPlayers.js
@@ -1,3 +1,12 @@
-const getPlayers = (socket, socketNameSpace) => {};
+const EVENT_TYPES = {
+  ON: 'getPlayers',
+  EMIT: 'resultGetPlayers'
+}
+
+const getPlayers = (socket, socketNameSpace) => {
+  socket.on(EVENT_TYPES.ON, () => {})
+};
+
+
 
 export { getPlayers };
