d970dda17910cb34ec1b80c05dafa27c78ef951a
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 16:56:34 +0900

    Implemetend helper fucntion to get players ID from socket

--- src/actions/helpers/getPlayersHelper.js
+++ src/actions/helpers/getPlayersHelper.js
@@ -1,4 +1,22 @@
+import socket from '../../socketHandlers/index';
+
+const SOCKET_EVENT_TYPES = {
+  EMIT: 'getPlayers',
+  ON: 'resultGetPlayers'
+}
+
 const getPlayersPromise = () => {
+  return new Promise((resolve, reject) => {
+    socket.on(SOCKET_EVENT_TYPES.ON, (data) => {
+      if(data.result.error) {
+        resolve(data.result.error);
+      } else {
+        const playerIds = data.result.data;
+        resolve(playerIds);
+      }
+    });
+    socket.emit(SOCKET_EVENT_TYPES.EMIT);
+  });
 };
 
 export { getPlayersPromise };
