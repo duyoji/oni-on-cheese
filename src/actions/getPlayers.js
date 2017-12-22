8683d4e40acd0772bf7edd4bc6ccc32921c72f50
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 19:57:43 +0900

    Implemented getPlayers

--- src/actions/getPlayers.js
+++ src/actions/getPlayers.js
@@ -1,6 +1,21 @@
-import 
+import { getPlayersHelper } from './helpers/getPlayersHelper'
 
-const getPlayers = () => {
+const getPlayers = (roomIds) => {
+  return async function(dispatch){
+    try {
+      const playerIds = await getPlayersHelper();
+      dispatch(getPlayersSuccess(playerIds));
+
+    } catch(err) {
+      console.error(err);
+    }
+  };
 };
+
+const getPlayersSuccess = (playerIds) => {{
+    type: 'GET_PLAYERS',
+    playerIds
+  }
+}
 
 export { getPlayers };
