7364fcaec4ba72a3ef01e2ab4e474811b1e93ddc
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 20:11:37 +0900

    Implement test env

--- src/__tests__/actions/getPlayers.js
+++ src/__tests__/actions/getPlayers.js
@@ -1,6 +1,18 @@
 import { getPlayers } from '../../actions/getPlayers';
+import sinon from 'sinon';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { getPlayersHelper } from '../../actions/helpers/getPlayersHelper';
+import { getDefaultState } from '../../reducers/index';
+import socket from '../../socketHandlers/index';
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
 
 describe('src/actions/getPlayers.js', () => {
+  afterAll(() => {
+    socket.disconnect();
+  })
   it('should dispatch action after receiving socket event', () => {
     
   });
