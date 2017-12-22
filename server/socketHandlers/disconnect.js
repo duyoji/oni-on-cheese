7190f4c92eab3c5314dafe55231dccb0b8f8f678
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-22 17:56:04 +0900

    Modified timeout seconds of watchPosition.

--- server/socketHandlers/disconnect.js
+++ server/socketHandlers/disconnect.js
@@ -1,6 +1,8 @@
 // See: ./index.js
 const disconnect = (socket, socketNamespace) => { // eslint-disable-line no-unused-vars
-  socket.on('disconnect', () => {});
+  socket.on('disconnect', () => {
+    // console.log('DISCONECT', socket.id, socketNamespace);
+  });
 };
 
 export { disconnect };
