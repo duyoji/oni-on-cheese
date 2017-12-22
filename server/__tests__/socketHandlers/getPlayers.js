1ef5b80b0e490497b808e312c7db38f4b8a2846b
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-22 15:12:44 +0900

    add test for adapter and passed

--- server/__tests__/socketHandlers/getPlayers.js
+++ server/__tests__/socketHandlers/getPlayers.js
@@ -15,7 +15,8 @@ describe('server/socketHanlders/getPlayers.js', () => {
     };
 
     let receivedEventTypeFromEmit = '';
-    let receivedDataFromEmit = '';
+    let receivedDataFromEmit = null;
+    const err = null;
     const expectedEmitData = ['player1', 'player2']
     const callbackForEmit = (eventType, data) => {
       receivedEventTypeFromEmit = eventType;
@@ -28,7 +29,7 @@ describe('server/socketHanlders/getPlayers.js', () => {
     );
     const nameSpace = createDummyNameSpace();
     nameSpace.adapter['clients'] = (fn) => {
-      fn(null, expectedEmitData);
+      fn(err, expectedEmitData);
     };
     
     getPlayers(socket, nameSpace);
@@ -37,5 +38,38 @@ describe('server/socketHanlders/getPlayers.js', () => {
     expect(receivedDataFromEmit).toEqual({result: {
       data: expectedEmitData
     }})
+  });
+
+  it('should handle err if there is', () => {
+    let receivedEventTypeFromOn = '';
+    const callbackForOn = (eventType, callback) => {
+      receivedEventTypeFromOn = eventType;
+      callback();
+    };
+
+    let receivedEventTypeFromEmit = '';
+    let receivedDataFromEmit = null;
+    const err = new Error('DUMMY_ERROR');
+    const expectedEmitData = undefined;
+    const callbackForEmit = (eventType, data) => {
+      receivedEventTypeFromEmit = eventType;
+      receivedDataFromEmit = data;
+    };
+
+    const socket = createDummySocket(
+      callbackForOn,
+      callbackForEmit
+    );
+    const nameSpace = createDummyNameSpace();
+    nameSpace.adapter['clients'] = (fn) => {
+      fn(err, expectedEmitData);
+    };
+    
+    getPlayers(socket, nameSpace);
+    expect(receivedEventTypeFromOn).toEqual('getPlayers');
+    expect(receivedEventTypeFromEmit).toEqual('resultGetPlayers');
+    expect(receivedDataFromEmit).toEqual({result: {
+      error: err
+    }});
   });
 });
--- server/socketHandlers/getPlayers.js
+++ server/socketHandlers/getPlayers.js
@@ -8,6 +8,10 @@ const EVENT_TYPES = {
 const getPlayers = (socket, socketNameSpace) => {
   socket.on(EVENT_TYPES.ON, () => {
     socketNameSpace.adapter.clients((err, players) => {
+      if(err){
+        socket.emit(EVENT_TYPES.EMIT, formatOutput({error: err}));
+        return;
+      }
       socket.emit(EVENT_TYPES.EMIT, formatOutput({data: players}));
     });
   });
