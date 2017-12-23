499c1a2bc010ebbe94ca3d658af03365ce01b050
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-23 14:48:33 +0900

    fixed for new state data structure

--- src/actions/helpers/getPlayersHelper.js
+++ src/actions/helpers/getPlayersHelper.js
@@ -5,17 +5,24 @@ const SOCKET_EVENT_TYPES = {
   ON: 'resultGetPlayers'
 }
 
-const getPlayersPromise = (roomId) => {
-  return new Promise((resolve, reject) => {
-    socket.on(SOCKET_EVENT_TYPES.ON, (data) => {
-      if(data.result.error) {
-        reject(data.result.error);
-      } else {
-        const playerIds = data.result.data;
-        resolve(playerIds);
-      }
+const getPlayersPromise = (rooms) => {
+  return rooms.map((room) => {
+    const roomId = room.roomId;
+    return new Promise((resolve, reject) => {
+      socket.on(SOCKET_EVENT_TYPES.ON, (data) => {
+        if(data.result.error) {
+          reject(data.result.error);
+        } else {
+          const playerIds = data.result.data;
+          const numberOfPlayers = playerIds.length;
+          resolve({
+            roomId,
+            numberOfPlayers
+          });
+        };
+      });
+      socket.emit(SOCKET_EVENT_TYPES.EMIT, roomId);
     });
-    socket.emit(SOCKET_EVENT_TYPES.EMIT, roomId);
   });
 };
 
