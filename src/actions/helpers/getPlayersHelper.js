19dd69ee1e8e21580cbc7951d3b01c38d61bf875
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2017-12-23 14:18:33 +0900

    fixed function

--- src/actions/helpers/getPlayersHelper.js
+++ src/actions/helpers/getPlayersHelper.js
@@ -9,10 +9,10 @@ const getPlayersPromise = (roomId) => {
   return new Promise((resolve, reject) => {
     socket.on(SOCKET_EVENT_TYPES.ON, (data) => {
       if(data.result.error) {
-        resolve(data.result.error);
+        reject(data.result.error);
       } else {
         const playerIds = data.result.data;
-        reject(playerIds);
+        resolve(playerIds);
       }
     });
     socket.emit(SOCKET_EVENT_TYPES.EMIT, roomId);
