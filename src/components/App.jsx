55cabb04869ee9cd5bfe00ad37b78aeeff23d9da
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-24 17:55:44 +0900

    Incorporated action and socketHandler into App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 import TopPage from '../containers/Top';
 import GameListPage from '../containers/GameListPage';
 import MapPage from '../containers/MapPage';
 import Header from '../containers/Header'
+import { addHandlerListener } from '../socketHandlers/connect';
 
 class App extends Component {
+  componentDidMount() {
+    addHandlerListener( socket => this.props.connectedToSocket(socket.id) );
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,5 +23,9 @@ class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  connectedToSocket: PropTypes.func.isRequired,
+};
 
 export default App;
