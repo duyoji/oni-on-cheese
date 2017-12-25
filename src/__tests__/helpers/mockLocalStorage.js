f0499916866415f52b19d257f84a2f2e43cc8df6
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-25 22:14:20 +0900

    Added comments in helpers/mockLocalStorage.js

--- src/__tests__/helpers/mockLocalStorage.js
+++ src/__tests__/helpers/mockLocalStorage.js
@@ -1,3 +1,5 @@
+// This file is for tests that use window.localStorage.
+// Thanks to this file, we can avoid errors such as `TypeError: Cannot read property 'getItem' of undefined`
 window.localStorage = window.localStorage || {};
 window.localStorage.setItem = window.localStorage.setItem || ((key, value) => {});
 window.localStorage.getItem = window.localStorage.getItem || ((key) => {});
