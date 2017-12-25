0cf9cbddb2feca3aa19ae649b6379ea884d8c8e1
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-25 13:52:13 +0900

    Created a test of components/modals/InputUserNameModal.js

---
+++ src/__tests__/components/modals/InputUserNameModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { shallow } from '../../helpers/configuredEnzymeWithAdapter';
+import InputUserNameModal from '../../../components/modals/InputUserNameModal';
+import { Modal, ModalHeader } from 'reactstrap';
+
+describe('src/components/modals/InputUserNameModal.jsx', () => {
+  it('shows modal if props.userName is undefined.', () => {
+    const wrapper = shallow( <InputUserNameModal
+      setUserName={craeteMockFunctionOfSetUserName()}
+    /> );
+
+    expect(wrapper.find(Modal).length).toEqual(1);
+    expect(wrapper.find(Modal).first().props().isOpen).toEqual(true);
+  });
+
+  it('shows nothing if props.userName is defined.', () => {
+    const wrapper = shallow( <InputUserNameModal
+      setUserName={craeteMockFunctionOfSetUserName()}
+      userName={'DUMMY_NAME'}
+    /> );
+
+    expect(wrapper.find(Modal).length).toEqual(1);
+    expect(wrapper.find(Modal).first().props().isOpen).toEqual(false);
+  });
+});
+
+const craeteMockFunctionOfSetUserName = (userName) => {};
