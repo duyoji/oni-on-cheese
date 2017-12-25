45e453cf315479439ae3c9a234846125f11c63a4
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-25 16:23:40 +0900

    Added some comments in MapLoader.jsx that includes implementation in hacky way.

--- src/components/loaders/MapLoader.jsx
+++ src/components/loaders/MapLoader.jsx
@@ -4,8 +4,12 @@ import { BeatLoader } from 'react-spinners';
 const MapLoader = (props) => {
   const style = props.style || {};
 
-  // Vertical middle.
+  // TODO: Fix this into proper way.
   if(!style.padding) {
+    // parseInt('300px', 10) => 300.
+    //   This is kind of hacky way.
+    //
+    // I want spinner/loader to be in center the container.
     const padding = style.height ? `${parseInt(style.height, 10) / 2 - 15}px` : '0';
     style.padding = padding;
   }
