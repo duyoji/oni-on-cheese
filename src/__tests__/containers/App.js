d919ae311c1f39bbc866f97c518e65a3067250de
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-12-27 15:27:26 +0900

    Added a code for socket.disconnect in container tests except for Header.js

--- src/__tests__/containers/App.js
+++ src/__tests__/containers/App.js
@@ -1,8 +1,12 @@
 import AppContainer from '../../containers/App';
 import AppComponent from '../../components/App';
-
+import socket from '../../socketHandlers/index';
 
 describe('src/containers/App', () => {
+  afterAll(() => {
+    socket.disconnect();
+  });
+
   it('should includes App component as a wrapped component.', () => {
     expect(AppContainer.WrappedComponent).toEqual(AppComponent);
   });
